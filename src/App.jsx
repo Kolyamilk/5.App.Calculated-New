dfdbd6d886a99a166afe8d7c7db3a4e38dee3d9a
Kolyamilk <Kolyamilk@mail.ru>
2024-02-20 11:24:01 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './Button'
 
 
 function App() {
-  const [screen, setScreen] = useState('state')
+  const [screen, setScreen] = useState([])
 
-  const newArr = []
-  const mas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
+  const newArr = [null, null, null, null, null, null, null, null, null, null]
+  const mas = [1, 2, 3]
+
   const buttons = ['AC', '+/-', '%', '/', 'x', '-', '+', '=', 1, 1, 2, 3, 2, 2, 2, 3, 2, 8, 6]
 
 
-  function num() {
+
+
+
+  //Функция узнать на что мы нажали
+  function handleClick(type) {
+    setScreen(type)
+    if(type==='AC'){
+      clearDisplay()
+    }
+  }
+  //функция добавляет нажатую кнопку в массив
+  function addNewNumber() {
+  }
+  //функция добавление текста на дисплей
+  function addNumDisplay() {
+  }
+
+  //Проверка на ширину цифр на дисплее
+  function checkNumLengthDisplay() {
     mas.map((num) => {
       for (let i = 0; i < mas.length; i++) {
         if (i === 16) {
@@ -21,9 +41,10 @@ function App() {
       newArr.push(num)
     })
   }
-  num()
-  function clearScreen() {
-
+  checkNumLengthDisplay()
+  // ДОДЕЛАТЬ Функция очищает дисплей
+  function clearDisplay() {
+    setScreen(null)
   }
   return (
     <>
@@ -31,25 +52,38 @@ function App() {
         <div className="containerPhone">
           <div className="content">
             <div className="content-screen">
-              <span>{newArr.join('')} {screen}</span>
+              {/* <span>{newArr.join('')}</span> */}
+              <span>{screen}</span>
             </div>
             <div className="content-buttons">
-              <button onClick={clearScreen}>AC</button>
-              <button>+/-</button>
-              <button>%</button>
-              <button>/</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
-              <button>5</button>
+              {/* <button onClick={clearDisplay} className='btnLightGrey'>AC</button>
+              <button className='btnLightGrey' onClick={handleClick}>+/-</button>
+              <button className='btnLightGrey'>%</button>
+              <button className='btnOrange'>/</button>
+              <button className='btnDarkGrey'>7</button>
+              <button className='btnDarkGrey'>8</button>
+              <button className='btnDarkGrey'>9</button>
+              <button className='btnOrange'>x</button>
+              <button className='btnDarkGrey'>4</button>
+              <button className='btnDarkGrey'>5</button>s
+              <button className='btnDarkGrey'>6</button>
+              <button className='btnOrange'>-</button>
+              <button className='btnDarkGrey'>1</button>
+              <button className='btnDarkGrey'>2</button>
+              <button className='btnDarkGrey'>3</button>
+              <button className='btnOrange'>+</button>
+              <button className='btn0 btnDarkGrey'>0</button>
+              <button className='btnDarkGrey'>.</button>
+              <button className='btnOrange'>=</button> */}
+              <Button onClick={() => handleClick(1)}>1</Button>
+              <Button onClick={() => handleClick(2)}>2</Button>
+              <Button onClick={() => handleClick(3)}>3</Button>
+              <Button onClick={() => handleClick(4)}>4</Button>
+              <Button onClick={() => handleClick(5)}>5</Button>
+              <Button onClick={() => handleClick(6)}>6</Button>
+              <Button onClick={() => handleClick('AC')}>AC</Button>
+
+
             </div>
           </div>
         </div>
